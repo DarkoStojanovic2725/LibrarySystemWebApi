0d74e17e9196449875b1854da75ad8b0ff834629
DarkoStojanovic2725 <darko1108stojanovic@gmail.com>
2021-09-14 16:18:25 +0200

    GetBookResponse changes

--- LibrarySystem.CQRS/Responses/Book/GetBookResponse.cs
+++ LibrarySystem.CQRS/Responses/Book/GetBookResponse.cs
@@ -5,6 +5,7 @@ namespace LibrarySystem.CQRS.Responses.Book
     public class GetBookResponse
     {
         public int Id { get; set; }
+        public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int Genre { get; set; }
